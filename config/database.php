86af75be94c4d6178458541b9f6e62434aa3d248
Xety <zoro.fmt@gmail.com>
2025-02-12 11:00:09 +0100

    update base to the latest laravel version

--- config/database.php
+++ config/database.php
@@ -145,7 +145,8 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'bds'), '_').'_database_'),
-            'serializer' => Redis::SERIALIZER_JSON,
+            'persistent' => env('REDIS_PERSISTENT', false),
+            //'serializer' => Redis::SERIALIZER_JSON,
         ],
 
         'default' => [
